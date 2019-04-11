2caa15aba9ed32ffd303c6af00cd4d6ce4aa7bcf
Lucas Mantovani <lucashmantovani@hotmail.com>
2019-04-11 10:54:09 -0300

    Changed read_board and find_path. Added search_children, insert_dfs, insert_bfs, insert_bstf, insert_a definitions.

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def read_board(filepath='board.txt'):
     with open(filepath, 'r') as board_file:
         dimensions_string = board_file.readline()
@@ -8,35 +9,62 @@ def read_board(filepath='board.txt'):
         n_cols = int(dimensions_string[1])
 
         board = []
-        for _ in range(n_rows):
-            row_string = board_file.readline()
-            row = []
-            for j in range(n_cols):
-                row.append(row_string[j])
-            board.append(row)
+        board = board_file.readlines()
+        for i in range(n_rows):
+            row = list(board[i])[:n_cols]
+            board[i] = row
 
-    return np.array(board)
+    return np.array(board), n_rows, n_cols
 
 
-def find_path(start_index, target_index, board, algorithm='dfs'):
+def search_children(node, board, n_rows, n_cols):
+    return []
+
+
+def insert_dfs(children, nodes):
+    pass
+
+
+def insert_bfs(children, nodes):
+    pass
+
+
+def insert_bstf(children, nodes):
+    pass
+
+
+def insert_a(children, nodes):
+    pass
+
+
+def find_path(start_index, target_index, board, n_rows, n_cols, algorithm='dfs'):
     assert algorithm in ['dfs', 'bfs', 'bstf', 'a*'], "The algorithm parameter should be dfs, bfs, bstf or a*"
 
     if algorithm is 'dfs':
-        pass
+        insert = insert_dfs
     elif algorithm is 'bfs':
-        pass
+        insert = insert_bfs
     elif algorithm is 'bstf':
-        pass
+        insert = insert_bstf
     else:
-        pass
+        insert = insert_a
 
-    path = []
+    # TODO: Implementation of searching and adding possible paths to list
+    nodes = [[start_index]]
+    # while nodes[0] != target_index or nodes != []:
+    #     children = search_children(nodes[0], board, n_rows, n_cols)
+    #     insert(children, nodes)
+
+    # path = nodes[0]
+    # path.reverse()
     return path
 
 
 if __name__ == "__main__":
-    board = read_board('board.txt')
-    start_index = np.argwhere(board == '#')
-    target_index = np.argwhere(board == '$')
-    path = find_path(start_index, target_index, board)
+    board, n_rows, n_cols = read_board('board.txt')
+    start_index = np.argwhere(board == '#')[0].tolist()
+    target_index = np.argwhere(board == '$')[0].tolist()
+    assert start_index is not [], "Start point not found at board"
+    assert target_index is not [], "Target point not found at board"
+    path = find_path(start_index, target_index, board, n_rows, n_cols)
     print(path)
